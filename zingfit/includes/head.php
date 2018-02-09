3cd11a1ad873356519881e86dbb93894a6ed55ac
PC-Arturo <arturo@pushcreativebrand.com>
2018-02-09 03:23:46 -0500

    More mobile styling

--- zingfit/includes/head.php
+++ zingfit/includes/head.php
@@ -27,7 +27,7 @@
 <body>
 
  <!-- Navigation -->
-<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
